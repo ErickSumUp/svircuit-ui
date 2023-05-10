7d71c50a13ec855aad70c7f388e73e32b699b2df
Erick <ekmoya@proton.me>
2023-05-10 00:16:25 -0400

    [NH] (feature): Update hover styles

--- src/lib/Input.svelte
+++ src/lib/Input.svelte
@@ -25,12 +25,15 @@
            class:readonly="{readonly}"
            class:disabled="{disabled}"
            class:input-has-prefix="{prefix.length > 0}"
-            class:select--invalid="{invalid && !disabled}"
+               class:input-has-suffix="{suffix.length > 0}"
            {placeholder}
+               type="text"
         >
-        <span class="suffix-wrapper">
-            {suffix}
-        </span>
+        {#if suffix.length > 0}
+            <span class="suffix-wrapper">
+                {suffix}
+            </span>
+        {/if}
     </div>
 </div>
 
@@ -81,18 +84,53 @@
         border: none;
         outline: 0;
         border-radius: 8px;
-        padding: 12px 16px;
+        padding: 12px 16px 12px 16px;
         transition: box-shadow 120ms ease-in-out,
              padding 120ms ease-in-out;
         width: 100%;
         margin: 0;
+
+        box-shadow: 0 0 0 1px #999999;
+    }
+    input:hover {
+        box-shadow: 0 0 0 1px #666666;
+    }
+    input:focus {
+        box-shadow: 0 0 0 4px #3063E9;
+    }
+    input:active {
+        box-shadow: 0 0 0 1px #3063E9;
     }
     input::placeholder {
         color: #999999;
         transition: color 120ms ease-in-out;
     }
+    .input--warning {
+        box-shadow: 0 0 0 1px #F5A720;
+    }
+    .input--warning:hover {
+        box-shadow: 0 0 0 1px #AD7A14;
+    }
+    .input--warning:focus {
+        box-shadow: 0 0 0 2px #F5A720;
+    }
+    .input--warning:active {
+        box-shadow: 0 0 0 1px #F5A720;
+    }
     .input--warning:not(:focus)::placeholder {
         color: #F5A720;
+    }
+    .input--invalid {
+        box-shadow: 0 0 0 1px #DE331D;
+    }
+    .input--invalid:hover {
+        box-shadow: 0 0 0 1px #B22426;
+    }
+    .input--invalid:focus {
+        box-shadow: 0 0 0 2px #DE331D;
+    }
+    .input--invalid:active {
+        box-shadow: 0 0 0 1px #DE331D;
     }
     .input--invalid:not(:focus)::placeholder {
         color: #DE331D;
@@ -106,8 +144,12 @@
     }
     .disabled {
         background-color: #E6E6E6;
+        box-shadow: 0 0 0 1px #999999;
     }
     .input-has-prefix {
         padding-left: 48px;
+    }
+    .input-has-suffix {
+        padding-right: 48px;
     }
 </style>
