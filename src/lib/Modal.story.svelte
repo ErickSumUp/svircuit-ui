f4081ffd37a55572cd8778530c6f0c46324b011e
Erick <erick.moya@sumup.com>
2023-07-07 11:16:45 +0200

    [NH] (feature): Add modal. Add modal story. Add healine story variant "four". Add button default value to help understand what is the expected when no values are given.

---
+++ src/lib/Modal.story.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import type { Hst } from '@histoire/plugin-svelte';
+	import Modal from './Modal.svelte';
+	import Button from './Button.svelte';
+	import Headline from './Headline.svelte';
+
+	export let Hst: Hst;
+	let showModal = false;
+</script>
+
+<Hst.Story title="Components/Modal" layout={{ type: 'grid', width: '100%' }}>
+	<Hst.Variant title="Default">
+		<Modal bind:showModal>
+			<div style="margin-bottom: 24px;">
+				<Headline as="h2" size="four">Hello World!</Headline>
+			</div>
+			<p>I am a modal</p>
+		</Modal>
+		<Button
+			variant="secondary"
+			on:click={() => {
+				showModal = true;
+			}}>Open modal</Button
+		>
+	</Hst.Variant>
+</Hst.Story>
