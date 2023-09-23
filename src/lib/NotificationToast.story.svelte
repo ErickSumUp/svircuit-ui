ed90606701b73e0ea95ad82ac1f800b67bcab229
Erick <erick.moya@sumup.com>
2023-09-23 19:55:12 -0300

    :zap: Add more histories showcase.

--- src/lib/NotificationToast.story.svelte
+++ src/lib/NotificationToast.story.svelte
@@ -6,18 +6,33 @@
 	export let Hst: Hst;
 
 	let variant: 'info' | 'success' | 'warning' | 'danger' = 'info';
-	let headline = 'This is a headline';
-	let body = 'This is a body';
+	let headline = '';
+	let body = 'This is a toast message.';
 </script>
 
 <Hst.Story title="Components/Notification Toast" layout={{ type: 'grid', width: '100%' }}>
-	<Hst.Variant title="Toast variants">
+	<Hst.Variant title="Default">
 		<NotificationToast {variant} {headline} {body} />
+	</Hst.Variant>
+	<Hst.Variant title="Toast variants">
+		<div style='display: flex; flex-direction: column; gap: 2rem'>
+			<NotificationToast variant="info" body="This is an info toast" />
+			<NotificationToast variant="success" body="This is a success toast" />
+			<NotificationToast variant="warning" body="This is a warning toast" />
+			<NotificationToast variant="danger" body="This is a danger toast" />
+		</div>
+	</Hst.Variant>
+	<Hst.Variant title="With headline">
+		<NotificationToast variant="info" headline="Information" body="You successfully updated your data." />
 	</Hst.Variant>
 	<svelte:fragment slot="controls">
 		<Hst.Text bind:value={headline} title="Headline" />
 		<Hst.Text bind:value={body} title="Body" />
-		<Hst.Select bind:value={variant} title="Variant" options={['info', 'success', 'warning', 'danger']} />
+		<Hst.Select
+			bind:value={variant}
+			title="Variant"
+			options={['info', 'success', 'warning', 'danger']}
+		/>
 		<pre>{JSON.stringify({ variant })}</pre>
 	</svelte:fragment>
 </Hst.Story>
