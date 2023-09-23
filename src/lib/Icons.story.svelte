d371e3cac21cfc6b89f6025abf75bc21668599b3
Erick <erick.moya@sumup.com>
2023-09-23 14:48:01 -0300

    :zap: Improve icon showcase

--- src/lib/Icons.story.svelte
+++ src/lib/Icons.story.svelte
@@ -6,6 +6,7 @@
 	import ChevronLeft from '$lib/icons/ChevronLeft.svelte';
 	import ChevronRight from '$lib/icons/ChevronRight.svelte';
 	import Info from '$lib/icons/Info.svelte';
+	import Notify from '$lib/icons/Notify.svelte';
 	export let Hst: Hst;
 
 	let size: '16' | '24' = '16';
@@ -14,24 +15,34 @@
 <Hst.Story title="Icons" layout={{ type: 'grid', width: '100%' }}>
 	<Hst.Variant title="Default">
 		<Confirm {size} />
-	</Hst.Variant>
-	<Hst.Variant title="Alert">
-		<Alert {size}/>
+		<Alert {size} />
+		<Confirm {size} />
+		<ChevronLeft {size} />
+		<ChevronRight {size} />
+		<Close {size} />
+		<Info {size} />
+		<Notify {size} />
 	</Hst.Variant>
 	<Hst.Variant title="Confirm">
-		<Confirm {size}/>
+		<Confirm {size} />
 	</Hst.Variant>
-	<Hst.Variant title="ChevronLeft">
-		<ChevronLeft {size}/>
-	</Hst.Variant>
-	<Hst.Variant title="ChevronRight">
-		<ChevronRight {size}/>
-	</Hst.Variant>
-	<Hst.Variant title="Close">
-		<Close {size}/>
+	<Hst.Variant title="Alert">
+		<Alert {size} />
 	</Hst.Variant>
 	<Hst.Variant title="Info">
-		<Info {size}/>
+		<Info {size} />
+	</Hst.Variant>
+	<Hst.Variant title="Notify">
+		<Notify {size} />
+	</Hst.Variant>
+	<Hst.Variant title="ChevronLeft">
+		<ChevronLeft {size} />
+	</Hst.Variant>
+	<Hst.Variant title="ChevronRight">
+		<ChevronRight {size} />
+	</Hst.Variant>
+	<Hst.Variant title="Close">
+		<Close {size} />
 	</Hst.Variant>
 	<svelte:fragment slot="controls">
 		<Hst.Select bind:value={size} options={['16', '24']} title="size" />
