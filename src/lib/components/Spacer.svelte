202334f5cac0646e64fc46bc99dc337d88bea113
Sebastián De Andraca <sebastian.deandraca@sumup.com>
2024-08-26 14:56:53 -0400

    spacer component and story

---
+++ src/lib/components/Spacer.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  /**
+   * Choose from size variants.
+   * @type {('s' | 'm' | 'l' | 'xl')}
+   */
+  export let size: 's' | 'm' | 'l' | 'xl' = 'm';
+</script>
+
+<div class="spacer spacer-{size}" />
+
+<style>
+  .spacer {
+    width: 100%;
+  }
+  .spacer-s {
+    height: var(--cui-spacings-byte);
+  }
+  .spacer-m {
+    height: var(--cui-spacings-mega);
+  }
+  .spacer-l {
+    height: var(--cui-spacings-tera);
+  }
+  .spacer-xl {
+    height: var(--cui-spacings-zetta);
+  }
+</style>
--- src/lib/index.js
+++ src/lib/index.js
@@ -5,6 +5,7 @@ export { default as Button } from './components/Button.svelte';
 export { default as Card } from './components/Card.svelte';
 export { default as Pagination } from './stories/Pagination.svelte';
 export { default as Input } from './components/Input.svelte';
+export { default as Spacer } from './components/Spacer.svelte';
 export { default as Table } from './components/Table.svelte';
 export { default as TBody } from './components/TBody.svelte';
 export { default as TD } from './components/TD.svelte';
