7b670d919a6287b5136fb7ad88c139b145f3d575
Erick <erick.moya@sumup.com>
2025-05-05 09:33:38 -0400

    :zap: Update styles file.

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,6 +1,6 @@
 import type { Preview } from '@storybook/svelte';
 
-import '../src/styles.css';
+import '../src/lib/styles.css';
 import { themes } from '@storybook/theming';
 
 const preview: Preview = {
