b7f10a6e38867597f2084f1b4623d03483626ca0
Erick <erick.moya@sumup.com>
2025-06-13 10:55:28 -0400

    :hammer: Make a new release to add stack gaps and alignment.

--- src/lib/components/Stack.svelte
+++ src/lib/components/Stack.svelte
@@ -3,15 +3,27 @@
 
   interface Props {
     vertical?: boolean;
+    align?: 'start' | 'center' | 'end' | 'baseline';
+    gap?: 'xs' | 's' | 'm' | 'l' | 'xl' | 'xxl';
     children?: Snippet;
-    align?: 'center';
     [key: string]: unknown;
   }
 
-  let { vertical = false, children }: Props = $props();
+  let { vertical = false, align = 'center', gap = 'm', children }: Props = $props();
 </script>
 
-<div class="base" class:vertical>
+<div class="base" class:vertical
+      class:start={align === 'start'}
+      class:center={align === 'center'}
+      class:end={align === 'end'}
+      class:baseline={align === 'baseline'}
+     class:xs={gap === 'xs'}
+     class:s={gap === 's'}
+     class:m={gap === 'm'}
+      class:l={gap === 'l'}
+      class:xl={gap === 'xl'}
+      class:xxl={gap === 'xxl'}
+>
   {@render children?.()}
 </div>
 
@@ -19,7 +31,6 @@
   .base {
     display: flex;
     flex-direction: column;
-    gap: 2rem;
     align-items: center;
     justify-content: center;
   }
@@ -34,5 +45,45 @@
     .vertical {
       flex-direction: column;
     }
+  }
+
+  .start {
+    align-items: flex-start;
+  }
+
+  .center {
+    align-items: center;
+  }
+
+  .end {
+    align-items: flex-end;
+  }
+
+  .baseline {
+    align-items: baseline;
+  }
+
+  .xs {
+    gap: var(--cui-spacings-bit);
+  }
+
+  .s {
+    gap: var(--cui-spacings-byte);
+  }
+
+  .m {
+    gap: var(--cui-spacings-kilo);
+  }
+
+  .l {
+    gap: var(--cui-spacings-mega);
+  }
+
+  .xl {
+    gap: var(--cui-spacings-giga);
+  }
+
+  .xxl {
+    gap: var(--cui-spacings-tera);
   }
 </style>
---
+++ src/stories/Stack.stories.svelte
@@ -0,0 +1,69 @@
+<script lang="ts" module>
+  import Stack from '$lib/components/Stack.svelte';
+  import { defineMeta } from '@storybook/addon-svelte-csf';
+  import type { ComponentProps } from 'svelte';
+
+  const { Story } = defineMeta({
+    title: 'Layout/Stack',
+    component: Stack,
+  });
+</script>
+
+<Story name="Base" args={{}}>
+  {#snippet template({ ...args }: ComponentProps<typeof Stack>)}
+    <Stack {...args}>
+      <div class="grey-box" style="width: 70px; padding-left: 30px; padding-top: 30px">First</div>
+      <div class="grey-box" style="width: 100px; height: 100px; text-align: center">Second</div>
+    </Stack>
+  {/snippet}
+</Story>
+
+<Story name="All Sizes">
+  {#snippet template()}
+    <Stack>
+      <Stack gap="s" vertical>
+        <div class="grey-box">One</div>
+        <div class="grey-box">Two</div>
+        <div class="grey-box">Three</div>
+        <div class="grey-box">Four</div>
+        <div class="grey-box">Five</div>
+      </Stack>
+      <Stack gap="m" vertical>
+        <div class="grey-box">One</div>
+        <div class="grey-box">Two</div>
+        <div class="grey-box">Three</div>
+        <div class="grey-box">Four</div>
+        <div class="grey-box">Five</div>
+      </Stack>
+      <Stack gap="l" vertical>
+        <div class="grey-box">One</div>
+        <div class="grey-box">Two</div>
+        <div class="grey-box">Three</div>
+        <div class="grey-box">Four</div>
+        <div class="grey-box">Five</div>
+      </Stack>
+      <Stack gap="xl" vertical>
+        <div class="grey-box">One</div>
+        <div class="grey-box">Two</div>
+        <div class="grey-box">Three</div>
+        <div class="grey-box">Four</div>
+        <div class="grey-box">Five</div>
+      </Stack>
+      <Stack gap="xxl" vertical>
+        <div class="grey-box">One</div>
+        <div class="grey-box">Two</div>
+        <div class="grey-box">Three</div>
+        <div class="grey-box">Four</div>
+        <div class="grey-box">Five</div>
+      </Stack>
+    </Stack>
+
+  {/snippet}
+</Story>
+
+<style>
+  .grey-box {
+    background-color: #d0d0d0;
+    padding: 10px;
+  }
+</style>
